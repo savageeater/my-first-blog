a83a45a75a8fcb7638f1e7dea743392ef6793ede
savageeater <lhs@naver.com>
2018-07-04 12:46:55 +0900

    Revert "changed the HTML for the site."
    This reverts commit 5c674bedad2a138903b693013f0df8df8188fdbd.

--- src/djangogirls/urls.py
+++ src/djangogirls/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include,url
+from django.urls import path
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'',include('blog.urls')),
-    
+    path('admin/', admin.site.urls),
 ]
